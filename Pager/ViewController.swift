d3823f67f9a202a1d6924d7b8c7adcd5d5b814ed
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2017-11-26 19:31:27 +0900

    Clean project

--- Pager/ViewController.swift
+++ Pager/ViewController.swift
@@ -25,16 +25,13 @@ class ViewController: UIViewController {
 
         let vc1 = UIViewController()
         vc1.title = "vc1"
-        vc1.view.backgroundColor = .black
+        vc1.view.backgroundColor = .red
         let vc2 = UIViewController()
         vc2.title = "vc2"
-        vc2.view.backgroundColor = .orange
+        vc2.view.backgroundColor = .blue
         let vc3 = UIViewController()
         vc3.title = "vc3"
-        vc3.view.backgroundColor = .blue
-//        let vc4 = UIViewController()
-//        vc4.title = "vc4"
-//        vc4.view.backgroundColor = .red
+        vc3.view.backgroundColor = .green
 
         let dataSource = PagerContentViewControllers(contentViewControllers: [vc1, vc2, vc3])
         vc.dataSource = dataSource
